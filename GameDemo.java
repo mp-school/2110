38cdc453ac862adb564f1b4e779423afb786b081
mp-school <mg770448@dal.ca>
2024-09-11 16:18:30 -0300

    grid printing is functional

--- GameDemo.java
+++ GameDemo.java
@@ -12,18 +12,38 @@ public class GameDemo{
         Scanner in = new Scanner(System.in); // Read in
 
 
-        String name = in.nextLine();
-        String colour = in.nextLine();
-        int positionX = in.nextInt();
-        int positionY = in.nextInt();
+        //String name = in.nextLine();
+        //String colour = in.nextLine();
+        //int positionX = in.nextInt();
+        //int positionY = in.nextInt();
 
-        Pieces gamePiece = new Pieces(name, colour, positionX, positionY);
 
-        System.out.print(gamePiece);
+        //Pieces gamePiece = new Pieces(name, colour, positionX, positionY);
+
+        //System.out.print(gamePiece);
         //gamePiece. // add slow piece and fast piece functions here
 
+        // new array to store object location
+        int size = 8;
+        String[][] pieceTracker = new String[size][size];
+
+
+        for(int i=0; i<size; i++){
+            for (int j=0; j<size; j++){
+                pieceTracker[i][j] = " - ";
+                System.out.print(pieceTracker[i][j]);
+                
+            }
+
+            System.out.println();
+
+        
+        }
+           
         
 
+        
+        
     }
 
 
