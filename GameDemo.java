37db15c6bfd29a02d7a5f8d4888c72ca6924394b
mp-school <mg770448@dal.ca>
2024-09-12 22:36:59 -0300

    v7 - added switch case and help

--- GameDemo.java
+++ GameDemo.java
@@ -9,7 +9,7 @@ public class GameDemo{
 Scanner in = new Scanner(System.in); // Read in
 
 
-//String name = in.nextLine();
+String name = in.nextLine();
 //String colour = in.nextLine();
 //int positionX = in.nextInt();
 //int positionY = in.nextInt();
@@ -25,18 +25,39 @@ int size = 8;
 String[][] pieceTracker = new String[size][size];
 
 
+// printing
 for(int i=0; i<size; i++){
     for (int j=0; j<size; j++){
         pieceTracker[i][j] = " - ";
-        System.out.print(pieceTracker[i][j]);
+        //System.out.print(pieceTracker[i][j]);
         
     }
 
-    System.out.println();
-
-
+    //System.out.println();
 
 }
+    
+
+// UI
+
+System.out.println("Enter a command (type help for details)");
+    switch (name) {
+        case "help":
+          System.out.println("Possible commands are as follows:");
+          System.out.println("create location [fast][flexible]: Creates a new piece.");
+          System.out.println("move location direction [spaces]: Moves a piece.");
+          System.out.println("print: Displays the board.");
+          System.out.println("help: Displays help");
+          System.out.println("exit: Exits the program.");
+          break;
+        case "print":
+          // code block
+          break;
+        default:
+          // code block
+      }
+
+
     }
 
 }
