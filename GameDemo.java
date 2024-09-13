1f39d67558d7643ba850ea16cd5c14e4ffcc0d2c
mp-school <mg770448@dal.ca>
2024-09-12 22:17:07 -0300

    went back to commit 5 print function

--- GameDemo.java
+++ GameDemo.java
@@ -5,60 +5,38 @@
 import java.util.*;
 
 public class GameDemo{
-
-    public static void main(String[] args){
-        // Create a new game
-
-        Scanner in = new Scanner(System.in); // Read in
+    public static void main (String[] args){
+Scanner in = new Scanner(System.in); // Read in
 
 
-        //String name = in.nextLine();
-        //String colour = in.nextLine();
-        
+//String name = in.nextLine();
+//String colour = in.nextLine();
+//int positionX = in.nextInt();
+//int positionY = in.nextInt();
 
 
-        //Pieces gamePiece = new Pieces(name, colour, positionX, positionY);
+//Pieces gamePiece = new Pieces(name, colour, positionX, positionY);
 
-        //System.out.print(gamePiece);
-        //gamePiece. // add slow piece and fast piece functions here
+//System.out.print(gamePiece);
+//gamePiece. // add slow piece and fast piece functions here
 
-        // new array to store object location
-        int size = 8;
-        String[][] pieceTracker = new String[size][size];
+// new array to store object location
+int size = 8;
+String[][] pieceTracker = new String[size][size];
 
 
-
-        boolean lineCheck = true;
-           
-        while(in.hasNext() == true){
-            String name = in.nextLine();
-            String colour = in.nextLine();
-            int positionX = in.nextInt();
-            int positionY = in.nextInt();
-            pieceTracker[positionX][positionY] = name;
-
-            if (name.equals("exit")){
-                lineCheck = false;
-            }
-
-            
-            if (name.equals("print")){
-             for(int i=0; i<size; i++){
-                for (int j=0; j<size; j++){
-                    pieceTracker[i][j] = " - ";
-                }
-            
-            }
-        }
-            
-            
-            
-    }
-
-    
-        
+for(int i=0; i<size; i++){
+    for (int j=0; j<size; j++){
+        pieceTracker[i][j] = " - ";
+        System.out.print(pieceTracker[i][j]);
         
     }
 
+    System.out.println();
+
+
+
+}
+    }
 
 }
