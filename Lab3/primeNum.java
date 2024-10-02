98cbb1662e5c0cca7920878d9c2d4df81a8736af
mp-school <mg770448@dal.ca>
2024-10-02 13:03:52 -0300

    Lab4 v1 - STarted and randomized array working

--- Lab3/primeNum.java
+++ Lab3/primeNum.java
@@ -1,3 +1,4 @@
+package Lab3;
 import java.util.*;
 
 public class primeNum {
@@ -8,7 +9,7 @@ public class primeNum {
     // using Sieve of Eratosthenes Algorithm
     public static void primeChecker(int size) {
 
-        int maxSize = 1000000;
+        int maxSize = 1000001;
 
         boolean[] primeCheck = new boolean[maxSize + 1];
         Arrays.fill(primeCheck, true);
@@ -38,12 +39,13 @@ public class primeNum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        long startTime = System.currentTimeMillis(); // Start timing before the lookup
-        primeChecker(1000);
+        
+        primeChecker(1000001);
         
         
         long n;
 
+        long startTime = System.currentTimeMillis(); // Start timing before the lookup
         while (true) {
             n = in.nextLong();
 
---
+++ Lab4/BubbleSort.java
@@ -0,0 +1,76 @@
+package Lab4; // comment out later
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+//TODO
+ //prompt the user to enter the value of n
+ //create an integer array of size n with random integers
+ //the range of random integers is from 1 to n
+
+
+public class BubbleSort{
+ public static void main(String[] args){
+ 
+    Scanner in = new Scanner(System.in);
+
+    while(true){
+    System.out.print("Input Array Size: ");
+    int n = in.nextInt(); // n array size
+
+    // randomized array 0 - n
+    int[]  randomArr = IntStream.generate(() -> new Random().nextInt(n)).limit(n-1).toArray();
+
+
+    // no sort
+    System.out.print("Non-Sorted Array: "); // test
+    System.out.print(Arrays.toString(randomArr)); // test
+    System.out.println(" ");
+
+    // sort
+    System.out.print("Sorted Array: "); // test
+    System.out.println(" ");
+    
+
+
+    System.out.println(" ");
+    //for (int[] print : sort.(randomArr[])){
+
+    //}
+    }
+
+ //long startTime, endTime, executionTime;
+ //startTime = System.currentTimeMillis();
+ //call to the sort method
+ //endTime = System.currentTimeMillis();
+ //executionTime = endTime - startTime;
+//display the executionTIme
+
+}
+ public static int[] sort(int[] arr){
+
+   boolean didSwap;
+   int temp;
+
+ for (int k = 1; k < arr.length; k++) {
+    didSwap = false;
+  
+   
+    for (int i = 0; i < arr.length - k; i++) {
+        if (arr[i] > arr[i + 1]) {
+
+             // Swap arr[j] and arr[j+1]
+             temp = arr[k];
+             arr[k] = arr[k + 1];
+             arr[k + 1] = temp;
+             didSwap = true;
+
+    }
+    }
+ 
+    
+
+   }
+   return arr;
+ }
+}
